c7769aea68f19c1af82c45f1b94db06cec570b35
nathan121lam <60200141+nathan121lam@users.noreply.github.com>
2020-12-04 18:56:51 -0500

    Started overview

--- GetFit/Overview.swift
+++ GetFit/Overview.swift
@@ -20,8 +20,84 @@ struct Overview: View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
     
-    var totalCaloriesBurnt {
-        
+    /*
+     MARK: - get total number of calories burnt
+     */
+    func totalCaloriesBurnt() -> Int {
+        var total = 0
+        for workout in allWorkouts {
+            total = total + Int(truncating: workout.calories ?? 0)
+        }
+        return total
+    }
+    
+    /*
+     MARK: - get average number of calores burnt
+     */
+    func averageCaloriesBurnt() -> Double {
+        let total = totalCaloriesBurnt()
+        let average = Double(total)/Double(allWorkouts.count)
+        let roundedAverage = Double(round(average*100)/100)
+        return roundedAverage
+    }
+    
+    /*
+     MARK: - get total number of exercise minutes
+     */
+    func totalExerciseMinutes() -> Int {
+        var total = 0
+        for workout in allWorkouts {
+            total = total + Int(truncating: workout.duration ?? 0)
+        }
+        return total
+    }
+    
+    /*
+     MARK: - get average number of exercise minutes
+     */
+    func averageExerciseMinutes() -> Double {
+        let total = totalCaloriesBurnt()
+        let average = Double(total)/Double(allWorkouts.count)
+        let roundedAverage = Double(round(average*100)/100)
+        return roundedAverage
+    }
+    
+    /*
+     MARK: - get average number of calories consumed
+     */
+    func averageCaloriesConsumed() -> Double {
+        var total = 0
+        for meal in allMeals {
+            total = total + Int(truncating: meal.calories ?? 0)
+        }
+        let average = Double(total)/Double(allMeals.count)
+        let roundedAverage = Double(round(average*100)/100)
+        return roundedAverage
+    }
+    
+    /*
+     MARK: - get most worked muscle category
+     */
+    func mostWorkedCategory() -> String {
+        return ""
+    }
+    
+    /*
+     MARK: - get least worked muscle category
+     */
+    func leastWorkedCategory() -> String {
+        return ""
+    }
+
+    /*
+     MARK: - get total weight lost/gained
+     */
+    func totalWeightLoss() -> Double {
+        let startWeight = allProgress[0].weight ?? 0
+        let endWeight = allProgress[allProgress.count-1].weight ?? 0
+        let weightLost = Double(truncating: endWeight) - Double(truncating: startWeight)
+        let roundedWeightLost = Double(round(weightLost*100)/100)
+        return roundedWeightLost
     }
 }
 
