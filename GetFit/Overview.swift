beae64889a70c364bdada484ece8ae8e1bb3a4e8
djgennello <djgennello@vt.edu>
2020-12-09 15:34:54 -0500

    Added more data to pdf

--- GetFit/Overview.swift
+++ GetFit/Overview.swift
@@ -105,7 +105,7 @@ struct Overview: View {
     func totalCaloriesBurnt() -> Int {
         var total = 0
         for workout in allWorkouts {
-            total = total + Int(truncating: workout.calories ?? 0)
+            total = total + (Int(truncating: workout.calories ?? 0) * Int(truncating: workout.timesDone ?? 0))
         }
         return total
     }
@@ -126,7 +126,7 @@ struct Overview: View {
     func totalExerciseMinutes() -> Int {
         var total = 0
         for workout in allWorkouts {
-            total = total + Int(truncating: workout.duration ?? 0)
+            total = total + (Int(truncating: workout.duration ?? 0) * Int(truncating: workout.timesDone ?? 0))
         }
         return total
     }
@@ -214,16 +214,94 @@ struct Overview: View {
             let attributes = [
                 NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 14)
               ]
-              let text = "Average Calories Burned per Workout: \(averageCaloriesBurnt())"
-              let textRect = CGRect(x: 100, // left margin
+            var text = "Average Calories Burned per Workout: \(totalWorkouts() != 0 ? "\(averageCaloriesBurnt()) cal" : "No workout completed")"
+            var textRect = CGRect(x: 100, // left margin
                                     y: 100, // top margin
-                                width: 400,
+                                width: pageWidth,
                                height: 20)
-
-              text.draw(in: textRect, withAttributes: attributes)
+            text.draw(in: textRect, withAttributes: attributes)
+            text = "Total Calories Burnt: \(totalCaloriesBurnt()) cal"
+            textRect = CGRect(x: 100, y: 120, width: pageWidth, height: 20);
+            text.draw(in: textRect, withAttributes: attributes)
+            text = "Average Minutes Exercised per Workout: \(totalWorkouts() != 0 ?  "\(averageExerciseMinutes())" : "No Workout Completed")"
+            textRect = CGRect(x: 100, y: 140, width: pageWidth, height: 20);
+            text.draw(in: textRect, withAttributes: attributes)
+            text = "Total Minutes Exercised: \(totalExerciseMinutes()) min"
+            textRect = CGRect(x: 100, y: 160, width: pageWidth, height: 20);
+            text.draw(in: textRect, withAttributes: attributes)
+            text = "Number of Workouts per each Muscle Category\n\tAbs: \(categories["Abs"] ?? 0)\n\tArms: \(categories["Arms"] ?? 0)\n\tBack: \(categories["Back"] ?? 0)\n\tCalves: \(categories["Calves"] ?? 0)\n\tChest: \(categories["Chest"] ?? 0)\n\tLegs: \(categories["Legs"] ?? 0)\n\tShoulders: \(categories["Shoulders"] ?? 0)"
+            textRect = CGRect(x: 100, y: 180, width: pageWidth, height: 160);
+            text.draw(in: textRect, withAttributes: attributes)
+            let weightChange = totalWeightLoss()
+            var descrWeightChange = ""
+            if weightChange < 0 {
+                descrWeightChange = "lost"
+            }
+            else if weightChange > 0 {
+                descrWeightChange = "gained"
+            }
+            text = "Total Weight Loss/Gain: \(weightChange) lbs \(descrWeightChange)"
+            textRect = CGRect(x: 100, y: 340, width: pageWidth, height: 20);
+            text.draw(in: textRect, withAttributes: attributes)
+            text = "Average Calories Consumed per Meal: \(averageCaloriesConsumed()) with \(allMeals.count)"
+            textRect = CGRect(x: 100, y: 360, width: pageWidth, height: 20);
+            text.draw(in: textRect, withAttributes: attributes)
+            
+            let temp = ProgressTab()
+            let chartURL = URL(string: "https://image-charts.com/chart?chtt=Weight+Loss+Progress&cht=lc&chg=20,50,1,1,333333&chd=a:\(temp.getProgressDataString())&chm=o,ff0000,0,-1,8.0&chs=350x150&chxt=y&chma=0,5,0,0")
+            let chartData = try! Data(contentsOf: chartURL!)
+            var image = UIImage(data: chartData)!
+            var nextTop = addImage(pageRect: pageRect, imageTop: 400, image: image)
+            
+            text = "Progress Photo Overview on Next Page"
+            textRect = CGRect(x: 100, y: Int(nextTop), width: pageWidth, height: 20);
+            text.draw(in: textRect, withAttributes: attributes)
+            
+            context.beginPage()
+            if (allProgress.count > 0) {
+                if allProgress[0].hasPhotoUrl == 1 {
+                    image = UIImage(named: allProgress[0].photoUrl ?? "ImageUnavailable")!
+                }
+                else {
+                    image = UIImage(data: allProgress[0].photo!)!
+                }
+                nextTop = addImage(pageRect: pageRect, imageTop: 0, image: image)
+                if (allProgress.count > 1) {
+                    let last = allProgress.count - 1
+                    if allProgress[last].hasPhotoUrl == 1 {
+                        image = UIImage(named: allProgress[last].photoUrl ?? "ImageUnavailable")!
+                    }
+                    else {
+                        image = UIImage(data: allProgress[last].photo!)!
+                    }
+                    _ = addImage(pageRect: pageRect, imageTop: nextTop, image: image)
+                }
+            }
         }
         return data
     }
+    
+    func addImage(pageRect: CGRect, imageTop: CGFloat, image: UIImage) -> CGFloat {
+        
+      // 1
+      let maxHeight = pageRect.height * 0.4
+      let maxWidth = pageRect.width * 0.8
+      // 2
+      let aspectWidth = maxWidth / image.size.width
+      let aspectHeight = maxHeight / image.size.height
+      let aspectRatio = min(aspectWidth, aspectHeight)
+      // 3
+      let scaledWidth = image.size.width * aspectRatio
+      let scaledHeight = image.size.height * aspectRatio
+      // 4
+      let imageX = (pageRect.width - scaledWidth) / 2.0
+      let imageRect = CGRect(x: imageX, y: imageTop,
+                             width: scaledWidth, height: scaledHeight)
+      // 5
+      image.draw(in: imageRect)
+      return imageRect.origin.y + imageRect.size.height
+    }
+
 }
 
 struct Overview_Previews: PreviewProvider {
